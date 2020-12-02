aea84f92b352ad59502a0c9479843447b11a9585
unknown <Shaditya2498@gmail.com>
2020-12-02 11:18:27 +0530

    gif

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {GrimNavbar} from './Components/GrimNavbar/GrimNavbar';
+import {GrimButtons} from './Components/GrimButtons/GrimButtons';
+import {GrimBackground} from './Components/GrimBackground/GrimBackground';
 import './App.css';
 
 
@@ -9,7 +11,11 @@ import './App.css';
 function App() {
   return (
     <div>
+    
     <GrimNavbar/>
+    <GrimBackground/>
+    <GrimButtons>Sign Up</GrimButtons>
+    
    </div>
   );
 }
---
+++ src/Components/GrimBackground/GrimBackground.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Field from './field.gif';
+
+import'./GrimBackground.css';
+
+
+export const GrimBackground = () =>{
+    return( <div >
+        <img src = {Field} alt='type' className='back'/>
+        </div>
+      
+       
+     
+    );
+}
